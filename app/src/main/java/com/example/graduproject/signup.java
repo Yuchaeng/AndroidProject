db6ba2c6e04f1137269d09c9ec7858f51342877b
chaeyi <shub29@naver.com>
2022-11-30 22:12:24 +0900

    회원가입 수정

--- app/src/main/java/com/example/graduproject/signup.java
+++ app/src/main/java/com/example/graduproject/signup.java
@@ -33,7 +33,6 @@ import java.util.regex.Pattern;
 
 public class signup extends AppCompatActivity {
     private FirebaseAuth mAuth; //파이어베이스 인증
-    private DatabaseReference mdbRef; //실시간 데이터베이스
 
     private TextInputLayout idInput;
     private TextInputLayout pwInput;
@@ -50,17 +49,15 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         mAuth = FirebaseAuth.getInstance();
-        mdbRef = FirebaseDatabase.getInstance().getReference();
 
         idInput = findViewById(R.id.idInput);
         pwInput = findViewById(R.id.pwInput);
         pwCheck = findViewById(R.id.pwcheck);
-        mailVerify = (Button)findViewById(R.id.mailVerify);
+        mailVerify = (Button) findViewById(R.id.mailVerify);
 
         idEdittext = findViewById(R.id.idEdittext);
         pwEditText = findViewById(R.id.pwEditText);
         pwOKcheck = findViewById(R.id.pwOKedit);
-
 
 
         idEdittext.addTextChangedListener(new TextWatcher() {
@@ -68,16 +65,17 @@ public class signup extends AppCompatActivity {
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
+
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-               if(isValidEmail(idEdittext.getText().toString())) {
-                   idInput.setErrorEnabled(false);
-               }
-               else {
-                   idInput.setError("필수입력/서울과기대 웹메일만 입력가능합니다.");
-               }
+                if (isValidEmail(idEdittext.getText().toString())) {
+                    idInput.setErrorEnabled(false);
+                } else {
+                    idInput.setError("필수입력/서울과기대 웹메일만 입력가능합니다.");
+                }
 
             }
+
             @Override
             public void afterTextChanged(Editable editable) {
 
@@ -89,12 +87,12 @@ public class signup extends AppCompatActivity {
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             }
+
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(isValidPw(pwEditText.getText().toString())) {
+                if (isValidPw(pwEditText.getText().toString())) {
                     pwInput.setErrorEnabled(false);
-                }
-                else {
+                } else {
                     pwInput.setError("필수입력/영문,숫자,특수문자를 조합해 8~15자리로 입력하세요.");
                 }
             }
@@ -112,12 +110,12 @@ public class signup extends AppCompatActivity {
 
 
             }
+
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(pwEditText.getText().toString().equals(pwOKcheck.getText().toString())) {
+                if (pwEditText.getText().toString().equals(pwOKcheck.getText().toString())) {
                     pwCheck.setErrorEnabled(false);
-                }
-                else {
+                } else {
                     pwCheck.setError("비밀번호가 일치하지 않습니다.");
                 }
             }
@@ -129,8 +127,6 @@ public class signup extends AppCompatActivity {
         });
 
 
-
-
         //회원가입 버튼 클릭 리스너
         mailVerify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,11 +135,10 @@ public class signup extends AppCompatActivity {
                 String password = pwEditText.getText().toString();
                 String passwordOk = pwOKcheck.getText().toString();
 
-                if(isValidEmail(email) && isValidPw(password) && password.equals(passwordOk)
-                &&!passwordOk.isEmpty()){
-                    createUser(email,password);
-                }
-                else {
+                if (isValidEmail(email) && isValidPw(password) && password.equals(passwordOk)
+                        && !passwordOk.isEmpty()) {
+                    createUser(email, password);
+                } else {
                     Toast.makeText(signup.this,"빈칸없이 조건에 맞게 입력하세요.",Toast.LENGTH_LONG).show();
                 }
             }
@@ -159,22 +154,20 @@ public class signup extends AppCompatActivity {
     private boolean isValidEmail(String inputEmail) {
         Pattern patternMail = Pattern.compile("^[a-zA-Z0-9]+@(seoultech.ac.kr)$");
         Matcher matcherMail = patternMail.matcher(inputEmail);
-        if(!inputEmail.isEmpty() && matcherMail.find()) {
+        if (!inputEmail.isEmpty() && matcherMail.find()) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
 
     //비밀번호 유효성 검사
     private boolean isValidPw(String inputPw) {
         Pattern patternPW = Pattern.compile("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@!%*#+?]).{8,15}.$");
-        Matcher matcherPW  = patternPW.matcher(inputPw);
+        Matcher matcherPW = patternPW.matcher(inputPw);
 
-        if(!inputPw.isEmpty() && matcherPW.find()) {
+        if (!inputPw.isEmpty() && matcherPW.find()) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
 
@@ -182,18 +175,30 @@ public class signup extends AppCompatActivity {
     private void createUser(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
-                    if(task.isSuccessful()) {
+                    if (task.isSuccessful()) {
                         //회원가입 성공
-                        Toast.makeText(signup.this,"회원가입 성공",Toast.LENGTH_LONG).show();
+                        Log.d(TAG,"signInWithEmail:success");
+                        Toast.makeText(signup.this, "인증 성공", Toast.LENGTH_LONG).show();
                         FirebaseUser user = mAuth.getCurrentUser();
-                    }
-                    else {
+                        gotoSetting(user);
+                    } else {
                         Log.w(TAG, "signInWithEmail:failure", task.getException());
-                        Toast.makeText(signup.this,"회원가입 실패",Toast.LENGTH_LONG).show();
+                        Toast.makeText(signup.this, "회원가입 실패", Toast.LENGTH_LONG).show();
 
                     }
                 });
     }
+
+    private void gotoSetting(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, profileInit.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
+
+
 
 
 
@@ -213,4 +218,3 @@ public class signup extends AppCompatActivity {
 //                });
 //    }
 
-}
