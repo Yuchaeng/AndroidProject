f1063ecd553b8a6da196d148f0d6e066d6b5a6a4
chaeyi <shub29@naver.com>
2022-12-04 18:25:39 +0900

    프로필 변경 액티비티 수정

--- app/src/main/java/com/example/graduproject/myChange.java
+++ app/src/main/java/com/example/graduproject/myChange.java
@@ -2,20 +2,37 @@ package com.example.graduproject;
 
 import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,19 +40,31 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class myChange extends AppCompatActivity {
 
     TextView picChange, whatName, whatBirth, whatGender, deptId, nickName, whenEmpty,
-    whatInterest, writeIntroduce;
+    whatInterest, writeIntroduce, nickNameChage, introduceChange, emptyTimeChange;
     Button signoutBtn;
+    ImageView profileImg;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseRef;
     private FirebaseUser mUser;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private Uri imageUri;
+    private String pathUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,31 +82,23 @@ public class myChange extends AppCompatActivity {
         whenEmpty = findViewById(R.id.whenEmpty);
         whatInterest = findViewById(R.id.whatInterest);
         writeIntroduce = findViewById(R.id.writeIntroduce);
+        nickNameChage = findViewById(R.id.nickNameChange);
+        introduceChange = findViewById(R.id.introduceChange);
+        emptyTimeChange = findViewById(R.id.emptyTimeChange);
 
         signoutBtn = findViewById(R.id.signoutBtn);
         picChange = findViewById(R.id.picChange);
-        //프로필 사진 변경
-        picChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        profileImg = findViewById(R.id.myPicture);
 
-            }
-        });
+        storage = FirebaseStorage.getInstance(); //스토리지 인스턴스
+        storageRef = storage.getReference(); //스토리지 참조
 
-
-        signoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                finish();
-                startActivity(new Intent(myChange.this,login.class));
-            }
-        });
-
+        //파이어베이스에서 데이터 가져와서 표시
         mDatabaseRef.child("userInfo").child(mUser.getUid()).addValueEventListener(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
+
                         userProfile profile = snapshot.getValue(userProfile.class);
                         String name = profile.getName();
                         String birth = profile.getBirth();
@@ -97,6 +118,7 @@ public class myChange extends AppCompatActivity {
                         whenEmpty.setText(emptyString);
                         whatInterest.setText(interestString);
                         writeIntroduce.setText(introduce);
+
                     }
 
                     @Override
@@ -106,12 +128,156 @@ public class myChange extends AppCompatActivity {
                 }
         );
 
+        //프로필 사진 띄우기
+        StorageReference pathRef = storageRef.child("UserProfile").child(mUser.getUid()+"_img");
+        if(pathRef!=null) { //profile.getProfileImageUrl() != null
+
+            pathRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(myChange.this)
+                            .load(uri)
+                            .into(profileImg);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Glide.with(myChange.this).load(R.drawable.cat_temp).into(profileImg);
+                }
+            });
+
+        }
+
+        //닉네임 변경
+        nickNameChage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(myChange.this,nickNameChange.class);
+                intent.putExtra("nickname",nickName.getText().toString());
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //프로필 변경
+        picChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                launcher.launch(intent);
+
+            }
+        });
+
+        //한줄소개 변경
+        introduceChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(myChange.this,introduce.class);
+                if(!writeIntroduce.getText().toString().equals("한줄소개를 작성해주세요.")) {
+                    intent.putExtra("oneline",writeIntroduce.getText().toString());
+                }
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        //로그아웃
+        signoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(myChange.this,login.class));
+            }
+        });
+
 
     } //oncreate 끝
 
 
+    //사진 선택 후 로드하기
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result)
+                {
+                    if (result.getResultCode() == RESULT_OK)
+                    {
+                        Log.e(TAG, "result : " + result);
+                        Intent intent = result.getData();
+                        imageUri = intent.getData();
+                        pathUri = getPath(intent.getData());
+                        Log.e(TAG, "uri : " + imageUri);
+                       //imageview.setImageURI(imageUri); -> glide 안쓰면 얘 쓰기
+                        Glide.with(myChange.this)
+                                .load(imageUri)
+                                .into(profileImg);
 
+                       uploadToFirebase(imageUri);
+                    }
 
+                }
+            });
+    //uri의 path를 가져오는...???
+    public String getPath(Uri uri) {
 
+        String[] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader = new CursorLoader(this, uri, proj, null, null, null);
+
+        Cursor cursor = cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+        return cursor.getString(index);
+    }
+
+    //프사 바꿀 때 올렸던 사진 있으면 삭제하고 업로드 -> 프사용 사진은 한 계정당 항상 한개만 저장됨
+    private void uploadToFirebase(Uri uri) {
+        StorageReference fileRef = storageRef.child("UserProfile").child(mUser.getUid()+"_img");
+        UploadTask uploadTask = fileRef.putFile(uri);
+
+        StorageReference desertRef = storageRef.child("UserProfile").child(mUser.getUid()+"_img");
+        desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e(TAG,exception.toString());
+                Toast.makeText(myChange.this,"업로드 실패",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        userProfile userProfile = new userProfile();
+                        userProfile.setProfileImageUrl(uri.toString());
+                        mDatabaseRef.child("userInfo").child(mUser.getUid()).child("profileImageUrl").removeValue();
+                        mDatabaseRef.child("userInfo").child(mUser.getUid()).child("profileImageUrl").setValue(uri.toString());
+                        Toast.makeText(myChange.this,"업로드 성공",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
 
 }
